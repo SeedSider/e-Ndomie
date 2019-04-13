ac1224a96b865ba0ba64279843562d870fbbaef2
Naufaldi Athallah Rifqi <naufalrifqi1602@gmail.com>
2019-04-13 14:43:31 +0700

    test

---
+++ src/main/java/adprogc4/endomie/EndomieApplication.java
@@ -0,0 +1,13 @@
+package adprogc4.endomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndomieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndomieApplication.class, args);
+    }
+
+}
