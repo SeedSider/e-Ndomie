7f430b059be734f82ff110467a4c0f38b7855d38
Naufaldi Athallah Rifqi <naufalrifqi1602@gmail.com>
2019-04-19 09:55:54 +0700

    udah bisa login with google

--- src/main/java/com/adprog6/endomie/EndomieApplication.java
+++ src/main/java/com/adprog6/endomie/EndomieApplication.java
@@ -1,13 +1,23 @@
 package com.adprog6.endomie;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EndomieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EndomieApplication.class, args);
     }
 
+    @RequestMapping(value = "/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 }
+
