027a1c3153c2299a2e5ce2195697670daed164c0
unknown <elioranathasya@gmail.com>
2019-04-21 16:17:40 +0700

    Dine In

---
+++ src/main/java/com/adprog6/endomie/order/DineIn.java
@@ -0,0 +1,9 @@
+package com.adprog6.endomie.order;
+
+public class DineIn implements OrderBehaviour {
+
+    @Override
+    public void setOrderType() { System.out.println("Dine In")};
+
+    //method lain coming soon
+}
