9ab6c3f2d57c290cafd02d77a76b2f362d42ab5d
Naufaldi Athallah Rifqi <naufalrifqi1602@gmail.com>
2019-04-21 17:13:02 +0700

    add navbar

--- src/main/java/com/adprog6/endomie/WebSecurityConfiguration.java
+++ src/main/java/com/adprog6/endomie/WebSecurityConfiguration.java
@@ -16,7 +16,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .disable()
                 .antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/", "/index.html")
+                .antMatchers("/", "/signin.html")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
---
+++ src/test/java/com/adprog6/endomie/EndomieApplicationTests.java
@@ -0,0 +1,10 @@
+package com.adprog6.endomie;
+
+import org.junit.Test;
+
+public class EndomieApplicationTests {
+    @Test
+    public void testEndomieApplicationHasMainMethod() {
+        EndomieApplication.main(new String[0]);
+    }
+}
