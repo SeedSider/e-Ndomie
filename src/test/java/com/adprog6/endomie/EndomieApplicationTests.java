290ce6d34ad77cc867ebaa03a9db6f9a79befe9d
unknown <elioranathasya@gmail.com>
2019-05-05 17:48:23 +0700

    revisi endomie app test

--- src/test/java/com/adprog6/endomie/EndomieApplicationTests.java
+++ src/test/java/com/adprog6/endomie/EndomieApplicationTests.java
@@ -1,10 +1,27 @@
+//package com.adprog6.endomie;
+//
+//import org.junit.Test;
+//
+//public class EndomieApplicationTests {
+//    @Test
+//    public void testEndomieApplicationHasMainMethod() {
+//        EndomieApplication.main(new String[0]);
+//    }
+//}
+
 package com.adprog6.endomie;
 
-import org.junit.Test;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EndomieApplicationTests {
+
     @Test
-    public void testEndomieApplicationHasMainMethod() {
-        EndomieApplication.main(new String[0]);
+    public void contextLoads() throws Exception {
     }
+
 }
