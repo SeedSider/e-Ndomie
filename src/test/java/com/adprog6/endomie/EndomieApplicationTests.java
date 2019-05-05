e79dc8aa2a564aa58bbcd56c243c14a8fb013135
Naufaldi Athallah Rifqi <naufalrifqi1602@gmail.com>
2019-05-05 14:47:28 +0700

    add tests

--- src/test/java/com/adprog6/endomie/EndomieApplicationTests.java
+++ src/test/java/com/adprog6/endomie/EndomieApplicationTests.java
@@ -1,10 +1,16 @@
 package com.adprog6.endomie;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EndomieApplicationTests {
+
     @Test
-    public void testEndomieApplicationHasMainMethod() {
-        EndomieApplication.main(new String[0]);
+    public void contextLoads() throws Exception {
     }
+
 }
---
+++ src/test/java/com/adprog6/endomie/controllers/MenuControllerTest.java
@@ -0,0 +1,22 @@
+package com.adprog6.endomie.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenuControllerTest {
+
+    @Autowired
+    private MenuController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
