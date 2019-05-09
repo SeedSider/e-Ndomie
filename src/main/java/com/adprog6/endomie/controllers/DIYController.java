2e33f9f77f36095071f3b45797246a054c3635f7
Naufaldi Athallah Rifqi <naufalrifqi1602@gmail.com>
2019-05-09 11:18:10 +0700

    test lagi

--- src/main/java/com/adprog6/endomie/controllers/DIYController.java
+++ src/main/java/com/adprog6/endomie/controllers/DIYController.java
@@ -1,12 +1,13 @@
 package com.adprog6.endomie.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DIYController {
-    @GetMapping("/diy")
-    public String login() {
-        return "diy";
-    }
+
+	@GetMapping("/diy")
+	public String diy() {
+		return "diy";
+	}
 }
