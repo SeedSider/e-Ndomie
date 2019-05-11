62859f7a80225715fc6837dba1ab1babf1447547
unknown <elioranathasya@gmail.com>
2019-05-11 15:54:54 +0700

    aneh masa ilang dummy & button2nya ga jalan??

--- src/main/java/com/adprog6/endomie/controllers/CartController.java
+++ src/main/java/com/adprog6/endomie/controllers/CartController.java
@@ -1,27 +1,42 @@
+//package com.adprog6.endomie.controllers;
+//
+//import com.adprog6.endomie.diy.CustomNoodle;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@RestController
+//public class CartController {
+//    List<CustomNoodle> isiCart=new ArrayList<>();
+//
+////    @PostMapping("/saveCart")
+////    public ResponseEntity<CustomNoodle> add
+//
+////    @GetMapping("/cart")
+////    public String cart() {
+////        return "cart";
+////    }
+//}
+//
+//
+
+
 package com.adprog6.endomie.controllers;
 
-import com.adprog6.endomie.diy.CustomNoodle;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
+@Controller
 public class CartController {
-    List<CustomNoodle> isiCart=new ArrayList<>();
-
-//    @PostMapping("/saveCart")
-//    public ResponseEntity<CustomNoodle> add
-
-//    @GetMapping("/cart")
-//    public String cart() {
-//        return "cart";
-//    }
+    @GetMapping("/cart")
+    public String cart() {
+        return "cart";
+    }
 }
-
 
--- src/main/java/com/adprog6/endomie/controllers/CartViewController.java
+++ src/main/java/com/adprog6/endomie/controllers/CartViewController.java
@@ -1,11 +1,11 @@
-package com.adprog6.endomie.controllers;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class CartViewController {
-    @GetMapping("/cart")
-    public String cart() {
-        return "cart";
-    }
-}
+//package com.adprog6.endomie.controllers;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class CartViewController {
+//    @GetMapping("/cart")
+//    public String cart() {
+//        return "cart";
+//    }
+//}
