ea43a5704473030af4a4704f921992f242cdf2ba
faisal hardin <faisalhardin23@gmail.com>
2019-05-12 22:09:23 +0700

    RESTful History

--- src/main/java/com/adprog6/endomie/controllers/HistoryController.java
+++ src/main/java/com/adprog6/endomie/controllers/HistoryController.java
@@ -1,55 +1,73 @@
 package com.adprog6.endomie.controllers;
 
+
+import com.adprog6.endomie.exception.ResourceNotFoundException;
 import com.adprog6.endomie.history.History;
 import com.adprog6.endomie.historyservice.HistoryService;
 import org.springframework.ui.Model;
 import com.adprog6.endomie.order.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/api")
 public class HistoryController {
 
-
+    @Autowired
     private HistoryService historyService;
 
-    @Autowired
-    private void  setHistoryService(HistoryService historyService) {
-        this.historyService = historyService;
+//    @Autowired
+//    private void  setHistoryService(HistoryService historyService) {
+//        this.historyService = historyService;
+//    }
+
+
+    @GetMapping(value = "/histories")
+    public Iterable<History> getAllHistories() {
+        return historyService.listAllHistory();
+    }
+//    public String list(Model model) {
+//        model.addAttribute("history", historyService.listAllHistory());
+//        System.out.println("returning history");
+//        return "history";
+//    }
+
+    @PostMapping("/history")
+    public History createHistory(@Valid @RequestBody History history) {
+        return historyService.saveHistory(history);
     }
 
-
-    @RequestMapping(value = "/histories", method = RequestMethod.GET)
-    public String list(Model model) {
-        model.addAttribute("history", historyService.listAllHistory());
-        System.out.println("returning history");
-        return "history";
+    @GetMapping("/history/{id}")
+    public History getHistoryById(@PathVariable(value = "id") Integer id) {
+        return Optional.ofNullable(historyService.getHistoryById(id)).
+                orElseThrow( () -> new ResourceNotFoundException("History", "id", id));
     }
 
-    @RequestMapping("history/{id}")
-    public String showProduct(@PathVariable Integer id,Model model) {
-        model.addAttribute("history", historyService.getHistoryById(id));
-        return "history";
+    @PutMapping("/history/{id}")
+    public History updateHistory(@PathVariable(value = "id") Integer id,
+                           @Valid @RequestBody History historyDetails) {
+
+        History history = Optional.ofNullable(historyService.getHistoryById(id))
+                .orElseThrow(() -> new ResourceNotFoundException("History", "id", id));
+
+        history.setUsername(historyDetails.getUsername());
+
+        History updatedHistory = historyService.saveHistory(history);
+        return updatedHistory;
     }
 
-    @RequestMapping("history/new")
-    public String newProduct(Model model) {
-        model.addAttribute("history", new History());
-        return "history";
-    }
+    @DeleteMapping("/history/{id}")
+    public ResponseEntity<?> deleteHistory(@PathVariable(value = "id") Integer id) {
+        History history = Optional.ofNullable(historyService.getHistoryById(id))
+                .orElseThrow(() -> new ResourceNotFoundException("History", "id", id));
 
-    @RequestMapping(value = "history", method = RequestMethod.POST)
-    public String saveHistory(History history) {
-        historyService.saveHistory(history);
-        return "redirect:/history/" + history.getIndex();
+        historyService.deleteHistory(history.getIndex());
 
-    }
-
-    @RequestMapping("history/delete/{id}")
-    public String delete(@PathVariable Integer id) {
-        historyService.deleteHistory(id);
-        return "redirect:/history";
+        return ResponseEntity.ok().build();
     }
 }
