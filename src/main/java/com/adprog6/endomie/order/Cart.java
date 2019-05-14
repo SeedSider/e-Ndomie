950928462a033daef13d86ae960aeaefb6af160a
Mikhael <mikhaelbuntaran@gmail.com>
2019-05-14 20:19:19 +0700

    fix double setter and add a getter

--- src/main/java/com/adprog6/endomie/order/Cart.java
+++ src/main/java/com/adprog6/endomie/order/Cart.java
@@ -18,8 +18,8 @@ public class Cart{
          orderBehaviour.orderType();
     }
 
-    public void setOrderBehaviour(OrderBehaviour orderBehaviour) {
-        this.orderBehaviour = orderBehaviour;
+    public OrderBehaviour getOrderBehaviour() {
+        return this.orderBehaviour;
     }
 
     //public void checkout(){}
