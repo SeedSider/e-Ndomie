2e601470d580b3d486aa87a195d7942ae16f743e
Usman Sidiq <usman.sidiq71@ui.ac.id>
2019-05-15 20:50:53 +0700

    fix conflict and error

--- src/main/java/com/adprog6/endomie/history/History.java
+++ src/main/java/com/adprog6/endomie/history/History.java
@@ -1,44 +1,44 @@
-package com.adprog6.endomie.history;
-
-import javax.persistence.*;
-
-
-import com.adprog6.endomie.order.Cart;
-
-@Entity
-public class History {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int index;
-
-    @Column
-    private String username;
-
-//    @OneToOne
-//    @JoinColumn(name = "cart_id")
-//    private Cart cart;
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-//    public Cart getCart() {
-//        return cart;
+//package com.adprog6.endomie.history;
+//
+//import javax.persistence.*;
+//
+//
+//import com.adprog6.endomie.order.Cart;
+//
+//@Entity
+//public class History {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private int index;
+//
+//    @Column
+//    private String username;
+//
+////    @OneToOne
+////    @JoinColumn(name = "cart_id")
+////    private Cart cart;
+//
+//    public int getIndex() {
+//        return index;
 //    }
 //
-//    public void setCart(Cart cart) {
-//        this.cart = cart;
+//    public void setIndex(int index) {
+//        this.index = index;
 //    }
-}
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+////    public Cart getCart() {
+////        return cart;
+////    }
+////
+////    public void setCart(Cart cart) {
+////        this.cart = cart;
+////    }
+//}
