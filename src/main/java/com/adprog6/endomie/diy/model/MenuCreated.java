05edcef0da17e174fd28ac6a8aa8b5f4ba40728b
Usman Sidiq <usman.sidiq71@ui.ac.id>
2019-05-15 16:58:22 +0700

    Done map to cart

---
+++ src/main/java/com/adprog6/endomie/diy/model/MenuCreated.java
@@ -0,0 +1,29 @@
+package com.adprog6.endomie.diy.model;
+
+public class MenuCreated {
+	private String id;
+	private String menu;
+	private String price;
+
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
